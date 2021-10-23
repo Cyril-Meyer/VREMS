8f1b3e7dc97f658b2737712f264fa7c6000140a6
Cyril-Meyer <contact@cyrilmeyer.eu>
2021-10-23 19:03:57 +0200

    create example using heinrich dataset

---
+++ example_heinrich.py
@@ -0,0 +1,25 @@
+import os
+import sys
+
+import numpy as np
+
+import utils
+import renderer
+
+# read data
+image = np.load("VREMS-data/heinrich/image_jrc_hela-2_crop3.npy")
+labels = np.load("VREMS-data/heinrich/labels_jrc_hela-2_crop3.npy")
+
+# image = np.load("VREMS-data/heinrich/image_jrc_jurkat-1_crop112.npy")
+# labels = np.load("VREMS-data/heinrich/labels_jrc_jurkat-1_crop112.npy")
+
+labels_array = []
+for v in np.unique(labels):
+    labels_array.append(utils.get_contours((labels == v)*1))
+
+utils.print_stack_info(image)
+utils.print_stack_info(np.array(labels_array))
+
+labels_colors = utils.get_colormap(len(np.unique(labels)), alpha=0.25)
+
+renderer.render_segmentation(image, labels_array, labels_colors)
