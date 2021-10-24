df4e5209020a1c3e3ad55f05931844c8c438ea66
Cyril-Meyer <contact@cyrilmeyer.eu>
2021-10-24 23:29:59 +0200

    update heinrich example (add all data paths)

--- example_heinrich.py
+++ example_heinrich.py
@@ -13,8 +13,17 @@ logging.basicConfig(level=logging.DEBUG)
 image = np.load("VREMS-data/heinrich/image_jrc_hela-2_crop3.npy")
 labels = np.load("VREMS-data/heinrich/labels_jrc_hela-2_crop3.npy")
 
+# image = np.load("VREMS-data/heinrich/image_jrc_hela-2_crop113.npy")
+# labels = np.load("VREMS-data/heinrich/labels_jrc_hela-2_crop113.npy")
+
+# image = np.load("VREMS-data/heinrich/image_jrc_hela-3_crop111.npy")
+# labels = np.load("VREMS-data/heinrich/labels_jrc_hela-3_crop111.npy")
+
 # image = np.load("VREMS-data/heinrich/image_jrc_jurkat-1_crop112.npy")
 # labels = np.load("VREMS-data/heinrich/labels_jrc_jurkat-1_crop112.npy")
+
+# image = np.load("VREMS-data/heinrich/image_jrc_macrophage-2_crop110.npy")
+# labels = np.load("VREMS-data/heinrich/labels_jrc_macrophage-2_crop110.npy")
 
 labels_array = []
 for v in np.unique(labels):
@@ -30,6 +39,6 @@ renderer.render_segmentation(image,
                              labels_colors,
                              rotation_speed=0.5,
                              view_distance=1.0,
-                             capture=1800,
-                             output="heinrich.mp4",
+                             # capture=1800,
+                             # output="heinrich.mp4",
                              video_fps=60)
